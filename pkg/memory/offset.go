dba427c2f6032b75f3d44cf9c886c72e317507d2
crazywh1t3b0y <115844608+crazywh1t3b0y@users.noreply.github.com>
2026-03-17 21:42:38 +0200

    Update offsets

--- pkg/memory/offset.go
+++ pkg/memory/offset.go
@@ -26,61 +26,61 @@ type Offset struct {
 
 func calculateOffsets(_ *Process) Offset {
 	// UnitTable
-	unitTableOffset := uintptr(0x1EAA3D0)
+	unitTableOffset := uintptr(0x1EAA350)
 
 	// UI
-	uiOffsetPtr := uintptr(0x1EBA0C2)
+	uiOffsetPtr := uintptr(0x1EBA042)
 
 	// Hover
-	hoverOffset := uintptr(0x1DFE090)
+	hoverOffset := uintptr(0x1DFE010)
 
 	// Expansion
-	expOffset := uintptr(0x1DFD4E0)
+	expOffset := uintptr(0x1DFD460)
 
 	// Party members offset
-	rosterOffset := uintptr(0x1EC06E0)
+	rosterOffset := uintptr(0x1EC0660)
 
 	// PanelManagerContainer
-	panelManagerContainerOffset := uintptr(0x1E14E38)
+	panelManagerContainerOffset := uintptr(0x1E14DB8)
 
 	// WidgetStates
-	WidgetStatesOffset := uintptr(0x1EE26F8)
+	WidgetStatesOffset := uintptr(0x1EE2678)
 
 	// Waypoints
-	WaypointTableOffset := uintptr(0x1D5C440)
+	WaypointTableOffset := uintptr(0x1D5C3C0)
 
 	// FPS
-	fpsOffset := uintptr(0x1D5C414)
+	fpsOffset := uintptr(0x1D5C394)
 
 	// KeyBindings
-	keyBindingsOffset := uintptr(0x19D55B4)
+	keyBindingsOffset := uintptr(0x19D5594)
 
 	// KeyBindings Skills
-	keyBindingsSkillsOffset := uintptr(0x1DFE180)
+	keyBindingsSkillsOffset := uintptr(0x1DFE100)
 
 	// QuestInfo
-	questInfoOffset := uintptr(0x1EC6D58)
+	questInfoOffset := uintptr(0x1EC6CD8)
 
 	// Terror Zones
-	tzOffset := uintptr(0x25B4A10)
+	tzOffset := uintptr(0x25B4990)
 
 	// Ping
-	pingOffset := uintptr(0x1DFD4E0)
+	pingOffset := uintptr(0x1DFD460)
 
 	// LegacyGraphics
-	legacyGfxOffset := uintptr(0x1EC6EFE)
+	legacyGfxOffset := uintptr(0x1EC6E7E)
 
 	// CharData
-	charDataOffset := uintptr(0x1E01790)
+	charDataOffset := uintptr(0x1E01710)
 
 	// Selected Char Name
-	selectedCharNameOffset := uintptr(0x1D53215)
+	selectedCharNameOffset := uintptr(0x1D53195)
 
 	// Last Game Name
-	lastGameNameOffset := uintptr(0x25FD370)
+	lastGameNameOffset := uintptr(0x25FD2F0)
 
 	// Last Game Password
-	lastGamePasswordOffset := uintptr(0x25FD3C8)
+	lastGamePasswordOffset := uintptr(0x25FD348)
 
 	return Offset{
 		UnitTable:                   unitTableOffset,
